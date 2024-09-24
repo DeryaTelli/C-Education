ead113bea26b9353bda8b2368c6317ea79a44319
Mobilyst <2002derya2002@gmail.com>
2024-09-24 14:58:54 +0300

    add the second homework

--- FirstLessonHomework2/FirstLessonHomework2/Program.cs
+++ FirstLessonHomework2/FirstLessonHomework2/Program.cs
@@ -10,6 +10,32 @@ namespace FirstLessonHomework2
     {
         static void Main(string[] args)
         {
+
+            //uc kenari klavyeden girilen ucgen ikiz kenar , eskenar ve cesit kenar ucgen olup olmadigini kontrol eden program 
+
+            Console.Write("Enter first edge of  triangle: ");
+            int firstEdge = Convert.ToInt32((Console.ReadLine()));
+
+            Console.Write("Enter second edge of  triangle: ");
+            int secondEdge = Convert.ToInt32((Console.ReadLine()));
+
+            Console.Write("Enter third edge of  triangle: ");
+            int thirdEdge = Convert.ToInt32((Console.ReadLine()));
+
+            if (firstEdge ==secondEdge && secondEdge ==thirdEdge && firstEdge==thirdEdge)
+            {
+                Console.WriteLine("equilateral triangle");
+            }
+            else if (firstEdge==secondEdge || secondEdge==firstEdge || firstEdge==thirdEdge)
+            {
+                Console.WriteLine("isosceles triangle");
+            }
+            else
+            {
+                Console.WriteLine("cesitragonal triangle");
+            }
+            Console.ReadLine();
+
         }
     }
 }
