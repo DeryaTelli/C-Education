62d20f2d7e7c879a63c11521af7757a803974845
Mobilyst <2002derya2002@gmail.com>
2024-10-05 02:24:42 +0300

    ref and out

---
+++ Lesson3OutAndRef/Lesson3OutAndRef/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3RefandOut
+{
+    internal class Program
+    {
+        public void swap(ref int x, ref int y)
+        {
+            int temp;
+            temp=x;
+            x=y;
+            y=temp;
+
+        }
+        public void getValue(out int x)
+        {
+            int temp = 5;
+            x=temp;
+        }
+
+        public void EnterValue(out int x, out int y)
+        {
+            Console.WriteLine("Enter the first value: ");
+            x=Convert.ToInt32((Console.ReadLine()));
+
+            Console.WriteLine("Enter the second value: ");
+            y=Convert.ToInt32(Console.ReadLine());
+
+        }
+        static void Main(string[] args)
+        {
+            //ref:
+            //Bir değişkenin referansını metot içinde kullanmak için kullanılır.
+            //Değişken, metot çağrılmadan önce tanımlanmalı ve bir değerle başlatılmalıdır.
+            //Metot içerisinde, değişkenin değeri değiştirilebilir ve değişiklikler çağrıldığı yere geri döner.
+
+            Program n = new Program();
+            int a = 100;
+            int b = 200;
+            n.swap(ref a, ref b);
+            Console.WriteLine("a: "+a+" b: "+b);
+
+            //out:
+            //Bir metottan birden fazla değer döndürmek için kullanılır.
+            //Değişken, metot çağrılmadan önce başlatılmak zorunda değildir. Metot içerisinde bir değer atanmalıdır.
+            //out değişkenleri, metot içinde kesinlikle bir değer almalıdır.
+            int c;
+            n.getValue(out c);
+            Console.WriteLine("c: "+ c);
+
+            int d, e;
+            n.EnterValue(out d, out e);
+            Console.WriteLine("d: "+d+" e: "+e);
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Lesson3OutAndRef/Lesson3OutAndRef/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Lesson3OutAndRef")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lesson3OutAndRef")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("6b6b2f0e-29d8-4b5b-9ae4-622aa522eb84")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
