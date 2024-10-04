ee632fbd49474d0964a6e210135317e30be6d503
Mobilyst <2002derya2002@gmail.com>
2024-10-04 22:37:16 +0300

    if else statement example

---
+++ Lesson1IfElseStatement/Lesson1IfElseStatement/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1IfElseStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            if (a>20)
+            {
+                Console.WriteLine("a is greater  than 20 ");
+            }
+            else
+            {
+                Console.WriteLine("a is  less than 20 ");
+            }
+
+
+            int i = 20, b = 30;
+            if (i>b)
+            {
+                Console.WriteLine("i is greater than b");
+
+            }else if (b>i)
+            {
+                Console.WriteLine("b is greater than i");
+            }
+            else
+            {
+                Console.WriteLine("b is equal i");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
