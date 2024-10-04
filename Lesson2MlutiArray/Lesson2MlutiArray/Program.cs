f4cbc65bf0873af4a71a093451dc5e9fec031e9d
Mobilyst <2002derya2002@gmail.com>
2024-10-05 00:17:05 +0300

    array example

---
+++ Lesson2MlutiArray/Lesson2MlutiArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2MlutiArray
+{
+    internal class Program
+    {
+        double getAverage(int[] arr)
+        {
+
+            double avg;
+            int sum = 0;
+            for (int a = 0; a<arr.Length; a++)
+            {
+                sum +=arr[a];
+
+            }
+            avg=(double)sum/arr.Length;
+            return avg;
+        }
+        static void Main(string[] args)
+        {
+            Program app = new Program();
+            int[] balance = new int[] { 1000, 2, 3, 17, 50 };
+            double avg;
+            avg=app.getAverage(balance);
+            Console.WriteLine("Average value is : {0}", avg);
+            Console.ReadLine();
+        }
+    }
+}
