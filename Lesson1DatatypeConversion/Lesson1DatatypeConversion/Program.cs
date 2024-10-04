a52d3b7cedbac3aad9165e3b352b5fc98d31eee5
Mobilyst <2002derya2002@gmail.com>
2024-10-04 22:26:15 +0300

    convert processing

---
+++ Lesson1DatatypeConversion/Lesson1DatatypeConversion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1DatatypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 100;
+            double myDouble = 2.25;
+            bool myBool = true;
+
+            Console.WriteLine(Convert.ToString(myInt)); // convert int to string 
+            Console.WriteLine(Convert.ToDouble(myInt)); //convert int to double 
+            Console.WriteLine(Convert.ToInt32(myDouble)); //convert double to int 
+            Console.WriteLine(Convert.ToString(myBool)); // convert bool to string 
+            Console.ReadLine();
+        }
+    }
+}
