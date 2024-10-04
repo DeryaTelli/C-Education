c9c37d81cc2e49b938a78643f1a729820ec20a54
Mobilyst <2002derya2002@gmail.com>
2024-10-04 22:17:08 +0300

    normal class and static class

---
+++ Lesson1GlobalVariable/Lesson1GlobalVariable/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1GlobalVariable
+{
+    internal class Program
+    {
+        class Bread
+         //bread classi icin nesne olusturulmak zorundadir 
+        {
+            public int counter;
+        }
+        static class Butter
+          //static classinin icindeki degiskenleride static olmali ve nesne olusturulmadan icerige erisim saglanabilir 
+        {
+            public static int counter;
+
+        }
+        static void Main(string[] args)
+        {
+            Bread b = new Bread();
+            b.counter=5;
+            Console.WriteLine("Bread: "+b.counter);
+
+            Butter.counter=7;
+            Console.WriteLine("Butter of counter: "+Butter.counter);
+            Console.ReadLine();
+        }
+    }
+}
