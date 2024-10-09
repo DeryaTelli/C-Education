f9a211279e112c03802e61fab7e49f6a268e71c8
Mobilyst <2002derya2002@gmail.com>
2024-10-09 20:13:39 +0300

    class example

---
+++ Lesson4ClassExample/Lesson4ClassExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4ClassExample
+{
+    class Car
+    {
+        string model;
+        string color;
+        int year;
+        static void Main(string[] args)
+        {
+            Car ford = new Car();
+            ford.model="Mustang";
+            ford.color="red";
+            ford.year=1996;
+
+            Car Opel =new Car();
+            Opel.model="Astra";
+            Opel.color="white";
+            Opel.year=2000;
+            Console.WriteLine(ford.model+" " + ford.color+ " "  + ford.year);
+            Console.WriteLine(Opel.model +" " + Opel.color+" " + Opel.year);
+            Console.ReadLine(); 
+        }
+    }
+}
