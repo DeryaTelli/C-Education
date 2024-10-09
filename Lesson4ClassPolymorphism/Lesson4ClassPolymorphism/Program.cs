2368c8e45574826417492b6eb71a6e25e6fc65be
Mobilyst <2002derya2002@gmail.com>
2024-10-09 21:44:50 +0300

    polymorphisim class

---
+++ Lesson4ClassPolymorphism/Lesson4ClassPolymorphism/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4ClassPolymorphism
+{
+    class Animal // base class (parent)
+    {
+        public virtual void animalSound()
+        {
+            Console.WriteLine("the animal makes a sound");
+        }
+    }
+    class Pig : Animal //derived calss (turetilmis class child)
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void animalSound() 
+        {
+            Console.WriteLine("The dog says: Bow bow");
+        }
+      
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal= new Animal(); // create a animal object 
+            Animal mypig= new Pig();// create a pig object 
+            Animal myDog= new Dog();//create a dog object 
+
+            myAnimal.animalSound();
+            myDog.animalSound();
+            mypig.animalSound();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson4ClassPolymorphism/Lesson4ClassPolymorphism/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Lesson4ClassPolymorphism")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lesson4ClassPolymorphism")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("e9793b83-428e-4c3f-bb5e-a797d9bc698d")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
