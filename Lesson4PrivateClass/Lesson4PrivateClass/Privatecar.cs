e42a6c403e80abb71d371a28fdf2f9711c47d886
Mobilyst <2002derya2002@gmail.com>
2024-10-09 21:18:06 +0300

    create get and set for that private class

---
+++ Lesson4PrivateClass/Lesson4PrivateClass/Privatecar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lesson4PrivateClass
+{
+    class Person
+    {
+        private string name;
+        private int age=19;
+        private string surName;
+
+        // get set tanimlama 1 
+        public string SurName // deger verilmeyen privatelar icin en mantikli tanimlama 
+        {
+            get; set;
+        }
+        // get set tanimlama 2 
+        public int Age // private degerin 
+        {
+            get { return age; } 
+            set { age = value; }
+            
+
+        }
+
+        public string Name
+        {
+            get { return name; } // private degeri goruntulemek icin kullanilir
+            set { name = value; } //private degeri degistirmek icin kullanilir 
+        }
+
+    }
+}
---
+++ Lesson4PrivateClass/Lesson4PrivateClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4PrivateClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1= new Person();
+            person1.Name="Liam";
+            Console.WriteLine(person1.Name);
+            Console.WriteLine(person1.Age);
+            person1.SurName="telli";
+            Console.WriteLine(person1.SurName);
+            Console.ReadLine();
+        }
+    }
+}
