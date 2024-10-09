fe7253ac810fd70cc9396d325a8f7903c19c98dd
Mobilyst <2002derya2002@gmail.com>
2024-10-09 21:36:24 +0300

    inheritance class example

---
+++ Lesson4InheritanceClass/Lesson4InheritanceClass/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4InheritanceClass
+{
+    class Car : Vehicle // katilim ile vehicleden turemis car sinifi vehicle sinifinin icinde her seye sabip olacaktir 
+    {
+        public string modelName = "Mustang";
+    }
+}
---
+++ Lesson4InheritanceClass/Lesson4InheritanceClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4InheritanceClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.honk();
+            Console.WriteLine(car.brand+ " "+car.modelName);
+            Console.ReadLine();
+        }
+    }
+}
