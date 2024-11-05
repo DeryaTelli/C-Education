390d178757d01be2556bc31004d2a6941ed8bdb7
Mobilyst <2002derya2002@gmail.com>
2024-11-05 18:19:00 +0300

    Constructor method rectangle

---
+++ ConstructorMethodexm3/ConstructorMethodexm3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorMethodexm3
+{
+    internal class Program
+    {
+        public class Rectangle
+        {
+            public double Width; 
+            public double Height;
+
+            public Rectangle(double width, double height)
+            {
+               Width=width;
+               Height=height;
+            }
+            public double CalculateArea()
+            {
+                return (Width*Height);
+            }
+            public double CalculatePerimeter()
+            {
+                return 2*(Width+Height);
+            }
+           
+
+        }
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(2, 3);
+            double area=r.CalculateArea();
+            Console.WriteLine("Area: " +area);
+            double perimeter=r.CalculatePerimeter();
+            Console.WriteLine("Perimeter: "+perimeter);
+            Console.ReadLine();
+        }
+    }
+}
