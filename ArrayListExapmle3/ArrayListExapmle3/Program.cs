6b7104a6b74e784429c2ac4db97671273db88037
Mobilyst <2002derya2002@gmail.com>
2024-11-05 22:04:10 +0300

    arraylist example 3

---
+++ ArrayListExapmle3/ArrayListExapmle3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListExapmle3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList() { 10, 20, 30, 50, 2 };
+            int sum = 0;
+            foreach (int i in list ) 
+            { 
+                sum += i;
+            }
+            double average = sum / list.Count;
+            Console.WriteLine("Average: "+average);
+            Console.ReadLine();
+
+        }
+    }
+}
