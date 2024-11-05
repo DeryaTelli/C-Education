4a831020c1a506661f8b392f6890c850bde5aac8
Mobilyst <2002derya2002@gmail.com>
2024-11-05 17:54:46 +0300

    constructor method example

---
+++ ConstructorMethodExm1/ConstructorMethodExm1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorMethodExm1
+{
+    internal class Program
+    {
+        class Person
+        {
+            public string Name;
+            public int Age;
+
+            //parametresiz constructor method
+            public Person()
+            {
+                Name = "Bilinmiyor";
+                Age = 0;
+            }
+
+            //parametreli constructor 
+            public Person(string name , int age)
+            {
+                Name = name;
+                Age=age;
+            }
+            public void PersonInfo()
+            {
+                Console.WriteLine("Name: "+Name+ " Age: "+Age);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Person p1 = new Person();
+            p1.PersonInfo();
+            Person p =new Person("Derya",22);
+            p.PersonInfo();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExamQuestion1/ExamQuestion1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamQuestion1
+{
+    internal class Program
+    {
+        public static int SpaceCount(string text )
+        {
+            int space = 0;
+            string text1;
+            for (int i=0; i<text.Length; i++)
+            {
+                text1=text.Substring(i, 1);
+                if (text1==" ")
+                {
+                    space++;
+                }
+            }
+            return space;
+
+        }
+        static void Main(string[] args)
+        {
+
+            //klavyeden girilen bir cumle icerisindeki bosluklarin sayisini spacecount fonksiyonu kullanarak hesaplayip bosluk
+            //sayisini ana fonksiton icersinde yazdiran program yaziniz
+            Console.Write("Plese enter the sentences: ");
+            string text = Console.ReadLine();
+           
+            Console.WriteLine("Text: "+text+ "Sentences contain spaces "+ SpaceCount(text));
+            Console.ReadLine();
+        }
+    }
+}
