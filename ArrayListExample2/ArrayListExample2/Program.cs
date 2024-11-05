9cd87f9ef99c68b9fb6dcabdba9b786e2bbd21a5
Mobilyst <2002derya2002@gmail.com>
2024-11-05 21:59:09 +0300

    arraylist exapmle  2

---
+++ ArrayListExample2/ArrayListExample2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListExample2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("derya");
+            list.Add(1);
+            list.Add(true);
+            list.Add(false);
+            list.Add(0.1);
+            foreach (var arrayList in list) 
+            {
+                Console.WriteLine("Values type: "+arrayList.GetType()+"\n Values: "+arrayList);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
