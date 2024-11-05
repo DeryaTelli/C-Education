c75e357697a8879dafb0c57abf4324ed669463b6
Mobilyst <2002derya2002@gmail.com>
2024-11-05 21:47:40 +0300

    ArrayList example 1

---
+++ ArrayListExample1/ArrayListExample1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListExample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dizi = { "Derya", "Zilan", "Irem", "Munto", "Mihri" };
+            ArrayList list = new ArrayList();
+            foreach (string names in dizi)
+            { 
+                list.Add(names);
+            }
+            foreach (string values in list) 
+            { 
+                Console.WriteLine(values);
+            }
+            Console.WriteLine("\nRemove some values");
+            list.Remove("Derya");
+            list.RemoveAt(2);
+            foreach (string vals in list) 
+            { 
+                Console.WriteLine(vals);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstructorMethodExm6/ConstructorMethodExm6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorMethodExm6
+{
+    internal class Program
+    {  
+        public class Product
+        {
+            public string name;
+            public double price;
+            public double discount;
+            public static double vat = 1.8;
+
+            public Product(string name , double price, double discount)
+            {
+                 this.name = name;
+                this.price = price;
+                this.discount = discount;
+
+            }
+            public double CalculateFinalPrice()
+            {
+                return (price - discount)*(1+vat);
+            }
+            public void ProductInfo()
+            {
+                Console.WriteLine("Name: "+name+ " Price: "+price+ " Discount: " +discount);
+            }
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            Product p1 = new Product("Cips", 45.5, 5);
+            double price=p1.CalculateFinalPrice();
+            p1.ProductInfo();
+            Console.WriteLine("Total price: "+price);
+            Console.ReadLine();
+        }
+    }
+}
