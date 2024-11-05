5afb4ac0c6f6e88df34b7df4ee79e88c459c9521
Mobilyst <2002derya2002@gmail.com>
2024-11-05 23:39:45 +0300

    ref out example 4

---
+++ RefOutExample3/RefOutExample3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefOutExample3
+{
+    internal class Program
+    {
+        static void FindMinMax(int[] numbers, out int min ,out int max)
+        {
+
+            min = numbers[0];
+            max = numbers[0];
+            foreach(int num in numbers)
+            {
+                if (num<min) min=num;
+                if (num>max) max=num;
+            }
+        }
+
+        static void ResetToZero(ref int number)
+        {
+            number=0;
+        }
+        static void Main(string[] args)
+        {
+            int[] values = { 2, 3, 1, 9, 4 };
+            FindMinMax(values, out int minValue, out int maxValue);
+            Console.WriteLine("min value: "+minValue);
+            Console.WriteLine("max value: "+maxValue);
+
+
+            int value = 100;
+            Console.WriteLine("Real value: "+value);
+            ResetToZero(ref value);
+            Console.WriteLine("Sifirlanmis Deger: "+value);
+            
+            Console.ReadLine();
+        }
+    }
+}
