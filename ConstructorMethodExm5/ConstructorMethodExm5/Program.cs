a72a7996e014d83efb4f1c9040bf2ed23ff5e397
Mobilyst <2002derya2002@gmail.com>
2024-11-05 20:31:26 +0300

    employee constructor example 5

---
+++ ConstructorMethodExm5/ConstructorMethodExm5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorMethodExm5
+{
+    internal class Program
+    {
+        public class Employee
+        {
+            public string name;
+            public double salary;
+
+            public Employee(string name)
+            {
+                this.name = name;
+                salary = 0.0;
+            }
+            public Employee(string name, double salary)
+            {
+                this.name = name;
+                this.salary = salary;
+            }
+            public void EmployeInfo()
+            {
+                Console.WriteLine("Name: "+name+ " Salary: "+salary);
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee("Deraa");
+            e1.EmployeInfo();
+            Employee e2 = new Employee("Defne", 10000);
+            e2.EmployeInfo();
+            Console.ReadLine();
+        }
+    }
+}
