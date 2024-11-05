14d963fb0a505b03c3c3e087bb1a1050e2f313fc
Mobilyst <2002derya2002@gmail.com>
2024-11-05 22:12:33 +0300

    Arraylist exapmle 5

---
+++ ArrayListExample5/ArrayListExample5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListExample5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList() { "Dera", "Ela", "Ali" };
+            list.Reverse(); // ali ela dera diye yazacaktir 
+            foreach (string s in list) 
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
