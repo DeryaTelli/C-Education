1c67f812f3cf02a3fbb05c0d4f82ab14f760ebf8
Mobilyst <2002derya2002@gmail.com>
2024-11-09 15:43:05 +0300

    exam 3 question B

---
+++ Exam3BQuestion/Exam3BQuestion/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam3BQuestion
+{
+    internal class Program
+    {
+        public class City
+        {
+            ArrayList city = new ArrayList() { "Ankara", "Istanbul", "Konya", "Karabuk", "Bartin" };
+
+            public  City(string cname)
+            {
+                if (city.Contains(cname)==true) {
+                    Console.WriteLine("Name of city found");
+
+                }
+                else
+                {
+                    Console.WriteLine("Name of city didn't find ");
+                }
+                
+            }
+            public void Delete_city(string cityName)
+            {
+                city.Remove(cityName);
+                Console.WriteLine("Delete name: "+cityName);
+                city.Reverse();
+                foreach (string list in city)
+                {
+                    Console.WriteLine("Reverse city list: "+list);
+                }
+            }
+
+           
+               
+        }
+        static void Main(string[] args)
+        {
+            //Ankara istanbul konya karabuk bartin sehirlerini iceren city isimli bir 
+            //arraylist tanimlayiniz 
+            //kullanicinin girdigi sehrin listede olup olmadigini find_city fonksiyonu ile 
+            //kontrol ediniz aranan isim liste icerisinde varsa bulundu liste icerisinde yoksa bulunamadi 
+            //bilgisini ekrana yazdiriniz 
+            //Delete_city fonksiyonu ile konya sehrini silerek listeyi tersten yazidiriniz 
+            Console.WriteLine("Enter city name please: ");
+            string cName = Console.ReadLine();
+            City find_city=new City(cName);
+            find_city.Delete_city("Konya");
+            Console.ReadLine();
+
+
+
+
+
+        }
+
+    }
+}
---
+++ Exam3BQuestion/Exam3BQuestion/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Exam3BQuestion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exam3BQuestion")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("70151e4f-612a-40d6-96f9-918bd59796f4")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
