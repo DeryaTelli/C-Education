da0f6eac33045c9952d051be281d575db5c6c50e
Mobilyst <2002derya2002@gmail.com>
2024-11-09 15:19:05 +0300

    exam 1 question B

---
+++ Exam1BQuestion/Exam1BQuestion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1BQuestion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //klavyeden girilen sayiya kadar olan tek sayilari ve
+            //toplamini ekrana yazan programi yaziniz
+
+            Console.WriteLine("Enter one number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            for (int i = 0; i < number; i++)
+            {
+                // Console.WriteLine(i);
+                if (i %2 != 0)
+                {
+                    Console.WriteLine("Odd number: " +i);
+                    result+=i;
+                     
+                }
+            }
+            Console.WriteLine("Odd total: "+result);
+            Console.ReadLine();
+        
+        }
+    }
+}
---
+++ Exam1BQuestion/Exam1BQuestion/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Exam1BQuestion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exam1BQuestion")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("a612b6b3-b01a-4b5c-9132-9a9db48ff9c0")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
